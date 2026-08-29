64e37feb5277bc78ab898bd14076c5da3f2d01ef
LayrtonJunior <layrton.dossantos@dcomp.ufs.br>
2026-08-28 21:58:57 -0300

    conta, precisa do main
    Fix indentation and syntax errors in Conta class methods.

--- conta.py
+++ conta.py
@@ -1,1 +1,20 @@
+class Conta:
+    
+    def__init__ (self,numero,nome,senha):
+      self.numero = numero
+      self.nome = nome 
+      self.senha = senha
+      self.saldo = 0
+    def depositar (self, valor):
+    if valor > 0
+       self saldo += valor 
+       return True
+    return False
+def sacar (self, valor)
+    if valor > 0 and valor <= self.saldo:
+       self saldo -= valor
+       return True
+    return False
+def consutar_salto(self):
+    return self.saldo
 
