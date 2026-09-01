a4690342f78ef736688698046b28023a680c7624
LayrtonJunior <layrton.dossantos@dcomp.ufs.br>
2026-09-01 16:15:24 -0300

    Fix indentation in Conta class methods

--- conta.py
+++ conta.py
@@ -1,20 +1,20 @@
 class Conta:
     
-  def __init__(self,numero,nome,senha):
+    def __init__(self,numero,nome,senha):
       self.numero = numero
       self.nome = nome 
       self.senha = senha
       self.saldo = 0
- def depositar (self, valor):
+    def depositar (self, valor):
     if valor > 0:
        self.saldo += valor 
        return True
     return False
-def sacar (self, valor):
-    if valor > 0 and valor <= self.saldo:
-       self.saldo -= valor
-       return True
-    return False
-def consultar_saldo(self):
-    return self.saldo
+    def sacar (self, valor):
+        if valor > 0 and valor <= self.saldo:
+            self.saldo -= valor
+            return True
+        return False
+    def consultar_saldo(self):
+        return self.saldo
 
