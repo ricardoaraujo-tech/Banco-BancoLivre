efb28eb0c868f861832e9fc76f5e9f60eef804e5
LayrtonJunior <layrton.dossantos@dcomp.ufs.br>
2026-09-01 15:28:35 -0300

    começo do codigo

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-def depositar (self, valor):
-    if valor > 0
-       self saldo += valor 
-       return True
-    return False
-def sacar (self, valor)
-    if valor > 0 and valor <= self.saldo:
-       self saldo -= valor
-       return True
-    return False
-def consutar_salto(self):
-    return self.saldo
+from client import Cliente
+from conta import Conta
