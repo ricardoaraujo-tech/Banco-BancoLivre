9c632bf8bcd0b8f61584a5f9244e02972e77f68e
LayrtonJunior <layrton.dossantos@dcomp.ufs.br>
2026-09-01 16:07:05 -0300

    Fix syntax errors in option handling

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ print("1 - Criar Cliente")
 print("2 - Criar Conta")
 print("3 - Sair")
 opcao = input ("escolha uma opção: ")
-if opcao == "1"
+if opcao == "1":
 #criar cliente
-elif opcao == "2"
+elif opcao == "2":
 #criar conta
-elif opcao == "3"
-#sair
+elif opcao == "3":
+#saindo...
