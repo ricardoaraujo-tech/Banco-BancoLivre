bba950bd697573bbaf76041fd72bc2988239a875
ricardoaraujo-tech <ricardo.araujo@dcomp.ufs.br>
2026-09-01 16:13:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-from cliente import Cliente
-from conta import Conta
-print("1 - Criar Cliente")
-print("2 - Criar Conta")
-print("3 - Sair")
-opcao = input ("escolha uma opção: ")
-if opcao == "1":
-#criar cliente
-elif opcao == "2":
-#criar conta
-elif opcao == "3":
-#saindo...
+class Cliente:
+    def __init__(self, nome, cpf, telefone, senha):
+        self.nome = nome
+        self.cpf = cpf
+        self.telefone = telefone
+        self.senha = senha
+
+def criar_cliente(nome, cpf, telefone, senha):
+    return Cliente(nome, cpf, telefone, senha)
+
+
+class Conta:
+    def __init__(self, numero, cliente):
+        self.numero = numero
+        self.cliente = cliente  # Associa a conta ao objeto Cliente
+        self.saldo = 0
+
+    def depositar(self, valor):
+        if valor > 0:
+            self.saldo += valor 
+            return True
+        return False
+
+    def sacar(self, valor):
+        if valor > 0 and valor <= self.saldo:
+            self.saldo -= valor
+            return True
+        return False
+
+    def consultar_saldo(self):
+        return self.saldo
