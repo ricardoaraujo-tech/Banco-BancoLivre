baeea5e116d24d2e52a8a09ae5b8f3695868cbb0
LayrtonJunior <layrton.dossantos@dcomp.ufs.br>
2026-09-01 15:22:44 -0300

    Update Cliente class and add client creation function
    Refactor Cliente class to include 'senha' attribute and add 'criar_cliente' function.

--- cliente.py
+++ cliente.py
@@ -1,6 +1,13 @@
 #Variáveis definidas -> Nome, CPF, Telefone
-    def__init__ (self,numero,nome,cpf):
-      self.telefone = telefone
-      self.nome = nome 
-      self.cpf = cpf
-    
+ #editei pfv dar uma olhada oque voces acham, ps:layrton
+class Cliente:
+    def __init__(self, nome, cpf, telefone, senha):
+        self.nome = nome
+        self.cpf = cpf
+        self.telefone = telefone
+        self.senha = senha
+
+def criar_cliente(nome, cpf, telefone):
+    cliente = Cliente(nome, cpf, telefone, senha=None)
+    return cliente
+for in conta return conta
