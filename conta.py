3dd145eb108a4a5794ef1020de020e123ae2a71b
pedroi01 <pedroinacio@academico.ufs.br>
2026-09-03 15:18:32 -0300

    Implementadas funções
    Refactor account class to improve variable names and add password verification for withdrawals.

--- conta.py
+++ conta.py
@@ -1,20 +1,28 @@
+#Variáveis definidas -> Número da conta, Titular da Conta, Senha e Saldo.
+
 class Conta:
-    
-    def __init__(self,numero,nome,senha):
+    def __init__(self, numero, titular, senha):
       self.numero = numero
-      self.nome = nome 
+      self.titular = titular
       self.senha = senha
-      self.saldo = 0
-    def depositar (self, valor):
-    if valor > 0:
-       self.saldo += valor 
-       return True
-    return False
-    def sacar (self, valor):
+      self.saldo = 0.0
+
+    def depositar(self, valor):
+       if valor > 0:
+            self.saldo += valor 
+            return True
+       return False
+
+    def sacar(self, valor, senha_informada):
+        if senha_informada != self.senha:
+            return False
         if valor > 0 and valor <= self.saldo:
             self.saldo -= valor
             return True
         return False
+
     def consultar_saldo(self):
         return self.saldo
+
+
 
